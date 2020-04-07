e60e71c42c3a14375688282f25f614ee6d1d4a3a
hrushkooleksii <hrushkooleksii@gmail.com>
2020-04-07 11:04:59 +0300

    validating project

--- src/js/js.js
+++ src/js/js.js
@@ -1,14 +1,13 @@
 let burgerbtn = document.querySelector('.header__burder-menu-span'),
-	closebtn = document.querySelector('.burder-close-icon'),
-	burgertoggle = document.querySelector('.bd-modal-burger'),
-	headermenu = document.querySelector('.header-top-menu');
+  closebtn = document.querySelector('.burder-close-icon'),
+  burgertoggle = document.querySelector('.bd-modal-burger'),
+  headermenu = document.querySelector('.header-top-menu');
 
-burgerbtn.onclick = function(){
-	burgertoggle.style.display = 'block';
-	headermenu.style.visibility = 'hidden';
+burgerbtn.onclick = function () {
+  burgertoggle.style.display = 'block';
+  headermenu.style.visibility = 'hidden';
 };
-closebtn.onclick = function(){
-	burgertoggle.style.display = 'none';
-	headermenu.style.visibility = 'visible';
+closebtn.onclick = function () {
+  burgertoggle.style.display = 'none';
+  headermenu.style.visibility = 'visible';
 };
-
