a15d50f5619cf5b5fb8fbeb82d07232b9700a20c
unknown <you@example.com>
2020-04-08 11:02:15 +0300

    updatemenu

--- src/js/js.js
+++ src/js/js.js
@@ -1,7 +1,11 @@
 let burgerbtn = document.querySelector('.header__burder-menu-span'),
   closebtn = document.querySelector('.burder-close-icon'),
   burgertoggle = document.querySelector('.bd-modal-burger'),
-  headermenu = document.querySelector('.header__top-menu');
+  headermenu = document.querySelector('.header__top-menu'),
+  btnlink = document.querySelector('.header__link--burger'),
+  nav = document.querySelector('.nav'),
+  display = document.querySelector('.display'),
+  header__link = document.querySelector('.header__link');
 
 burgerbtn.onclick = function () {
   burgertoggle.style.display = 'block';
@@ -10,4 +14,17 @@ burgerbtn.onclick = function () {
 closebtn.onclick = function () {
   burgertoggle.style.display = 'none';
   headermenu.style.visibility = 'visible';
+  location.reload();
 };
+btnlink.onclick = function(){
+	nav.style.display = 'none';
+	btnlink.style.display = 'none';
+	display.style.display = 'block';
+}
+header__link.onclick = function(){
+	burgertoggle.style.display = 'block';
+	headermenu.style.visibility = 'hidden';
+	nav.style.display = 'none';
+	display.style.display = 'block';
+	btnlink.style.display = 'none';
+}
