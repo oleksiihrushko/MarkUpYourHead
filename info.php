87f3d53563f311b666bb8935a833b3fda0d2407e
hrushkooleksii <hrushkooleksii@gmail.com>
2020-04-08 13:02:33 +0300

    adding php

---
+++ info.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_POST['btn'])) {
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+
+	$to      = 'reintdenis@gmail.com';
+	$subject = 'Перезвонить';
+	$message = 'Номер телефона - ' . $phone;
+	$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	
+
+	$success = mail($to, $subject, $message, $headers);
+	if (!$success) {
+	    $errorMessage = error_get_last()['message'];
+	}else{
+		echo "Спасибо за заказ $name".'<br>';
+		echo "Мы вам перезвоним в течение 10 минут"."<br>";
+		echo "<a href='index.html'>Вернуться на сайт</a>";
+	}
+}
+?>
